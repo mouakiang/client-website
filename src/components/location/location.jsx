3aa1aaad4ff9f34fd6e95dca3da14a65b92d1fe7
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-12-06 14:51:46 -0500

    signed it

--- src/components/location/location.jsx
+++ src/components/location/location.jsx
@@ -1,7 +1,8 @@
-export default function Location () {
-    return (
-        <div className="locationPage">
-            <h2>Location Page</h2>
-        </div>
-    )
+export default function Location() {
+  return (
+    <div className="locationPage">
+      <h2>Location Page</h2>
+      <p>Tinkerings by Greg</p>
+    </div>
+  );
 }
