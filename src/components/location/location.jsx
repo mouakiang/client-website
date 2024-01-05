939d28b528ce528478ed59dc248d89f2485b97b6
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-01-05 16:03:19 -0500

    updating hours

--- src/components/location/location.jsx
+++ src/components/location/location.jsx
@@ -20,39 +20,62 @@ export default function Location() {
   //still not able to update the map styles on the google cloud website. annoying.
 
   return (
-    <div className="locationPage">
-      <h2>Location Page</h2>
-      <APIProvider apiKey={apiKey}>
-        <div className="googleMaps">
-          <Map zoom={17} center={position} mapId={mapId}>
-            <AdvancedMarker position={position} onClick={() => setOpen(true)}>
-              <Pin
-                background={"green"}
-                glyphColor={"#0D5A27"}
-                borderColor={"#0D5A27"}
-              />
-            </AdvancedMarker>
-            {open ? (
-              <InfoWindow
-                position={position}
-                onCloseClick={() => setOpen(false)}
-              >
-                <Link
-                  to={stevenKiaLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Steven Kia
-                </Link>
-                <br />
+    <>
+      <div className="locationPage">
+        <h2>Location Page</h2>
+        <APIProvider apiKey={apiKey}>
+          <div className="map-frame">
+            <span className="map-text">
+              <p className="address">
                 3340 S Main St <br />
                 Harrisonburg, VA 22801
-              </InfoWindow>
-            ) : null}
-          </Map>
-        </div>
-      </APIProvider>
-      <p>Tinkerings by Greg</p>
-    </div>
+              </p>
+
+              <span className="hours">
+                <h5 className="hours">Hours:</h5> <br />
+                <p className="hours">Monday: 10AM-7PM</p> <br />
+                <p className="hours">Tuesday: 12:30PM-7PM</p> <br />
+                <p className="hours">Wednesday: 10AM-5:30PM</p> <br />
+                <p className="hours">Thursday: CLOSED</p> <br />
+                <p className="hours">Friday: 10AM-7PM</p> <br />
+                <p className="hours">Saturday: 9AM-6PM</p> <br />
+                <p className="hours">Sunday: CLOSED</p> <br />
+              </span>
+            </span>
+            <div className="googleMaps">
+              <Map zoom={17} center={position} mapId={mapId}>
+                <AdvancedMarker
+                  position={position}
+                  onClick={() => setOpen(true)}
+                >
+                  <Pin
+                    background={"green"}
+                    glyphColor={"#0D5A27"}
+                    borderColor={"#0D5A27"}
+                  />
+                </AdvancedMarker>
+                {open ? (
+                  <InfoWindow
+                    position={position}
+                    onCloseClick={() => setOpen(false)}
+                  >
+                    <Link
+                      to={stevenKiaLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Steven Kia
+                    </Link>
+                    <br />
+                    3340 S Main St <br />
+                    Harrisonburg, VA 22801
+                  </InfoWindow>
+                ) : null}
+              </Map>
+            </div>
+          </div>
+        </APIProvider>
+      </div>
+    </>
   );
 }
