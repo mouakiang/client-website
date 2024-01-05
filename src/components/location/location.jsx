1ec8a566a7f801762896d4380e8be1adb26e4270
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2024-01-05 15:15:38 -0500

    changes

--- src/components/location/location.jsx
+++ src/components/location/location.jsx
@@ -1,7 +1,58 @@
-export default function Location () {
-    return (
-        <div className="locationPage">
-            <h2>Location Page</h2>
+import { useState } from "react";
+import "./location.css";
+import { Link } from "react-router-dom";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
+
+export default function Location() {
+  const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+  const mapId = process.env.REACT_APP_MAP_ID;
+  const position = { lat: 38.403256, lng: -78.909981 };
+  const stevenKiaLink =
+    "https://www.google.com/maps/place/Steven+Kia/@38.403456,-78.909981,17z/data=!4m6!3m5!1s0x89b49394272b7d37:0x3caba5359e4e59c1!8m2!3d38.4033133!4d-78.909974!16s%2Fg%2F1tdcsdvt?hl=en-US&entry=ttu";
+  const [open, setOpen] = useState(false);
+
+  //still not able to update the map styles on the google cloud website. annoying.
+
+  return (
+    <div className="locationPage">
+      <h2>Location Page</h2>
+      <APIProvider apiKey={apiKey}>
+        <div className="googleMaps">
+          <Map zoom={17} center={position} mapId={mapId}>
+            <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+              <Pin
+                background={"green"}
+                glyphColor={"#0D5A27"}
+                borderColor={"#0D5A27"}
+              />
+            </AdvancedMarker>
+            {open ? (
+              <InfoWindow
+                position={position}
+                onCloseClick={() => setOpen(false)}
+              >
+                <Link
+                  to={stevenKiaLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Steven Kia
+                </Link>
+                <br />
+                3340 S Main St <br />
+                Harrisonburg, VA 22801
+              </InfoWindow>
+            ) : null}
+          </Map>
         </div>
-    )
+      </APIProvider>
+      <p>Tinkerings by Greg</p>
+    </div>
+  );
 }
