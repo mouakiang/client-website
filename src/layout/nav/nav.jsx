d5c7bfbbb0d4bc70c79e58b52d0822ced6f47145
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-01-15 15:39:00 -0500

    added mobile menu

--- src/layout/nav/nav.jsx
+++ src/layout/nav/nav.jsx
@@ -1,43 +1,100 @@
 import "./nav.css";
-import React from "react";
+import React, { Fragment, useState } from "react";
 import logo from "../../assets/SV.png";
 import contactImg from "../../assets/contact.png";
+import burger from "../../assets/burger-bar.png";
 import { NavLink, Link } from "react-router-dom";
 
 export function Nav() {
-  return (
-    <div className="topnav" id="myTopNav">
-        <img src={logo} alt="logo" className="navLogo" />
-      
+  const [showMenu, setShowMenu] = useState(false);
 
-      <nav className="navLinks">
-        <h3>
-          <NavLink to="/" exact="true" activeClassName="active">
-            HOME
-          </NavLink>
-        </h3>
-        <h3>
-          <NavLink to="/about" activeClassName="active">
-            ABOUT
-          </NavLink>
-        </h3>
-        <h3>
-          <NavLink to="/location" activeClassName="active">
-            LOCATION
-          </NavLink>
-        </h3>
-        <h3>
-          <NavLink to="/appointment" exact="true" activeClassName="active">
-            APPOINTMENT
-          </NavLink>
-        </h3>
+  return (
+    <>
+      <nav className="topnav" id="myTopNav">
+        <img src={logo} alt="logo" className="navLogo" />
+        <div className="desktop-menu">
+          <h3>
+            <NavLink to="/" exact="true" activeClassName="active">
+              HOME
+            </NavLink>
+          </h3>
+          <h3>
+            <NavLink to="/about" activeClassName="active">
+              ABOUT
+            </NavLink>
+          </h3>
+          <h3>
+            <NavLink to="/location" activeClassName="active">
+              LOCATION
+            </NavLink>
+          </h3>
+          <h3>
+            <NavLink to="/appointment" exact="true" activeClassName="active">
+              APPOINTMENT
+            </NavLink>
+          </h3>
+          <button className="navContactBtn">
+            <Link to="contact">
+              <img src={contactImg} alt="contact" className="contactBtnImg" />
+              Contact Me
+            </Link>
+          </button>
+        </div>
+        <img
+          src={burger}
+          alt="burgerMenu"
+          className="burgerMenu"
+          onClick={() => setShowMenu(!showMenu)}
+        />
+        <div
+          className="mobile-menu"
+          style={{ display: showMenu ? "flex" : "none" }}
+        >
+          <h3>
+            <NavLink
+              to="/"
+              exact="true"
+              activeClassName="active"
+              onClick={() => setShowMenu(false)}
+            >
+              HOME
+            </NavLink>
+          </h3>
+          <h3>
+            <NavLink
+              to="/about"
+              activeClassName="active"
+              onClick={() => setShowMenu(false)}
+            >
+              ABOUT
+            </NavLink>
+          </h3>
+          <h3>
+            <NavLink
+              to="/location"
+              activeClassName="active"
+              onClick={() => setShowMenu(false)}
+            >
+              LOCATION
+            </NavLink>
+          </h3>
+          <h3>
+            <NavLink
+              to="/appointment"
+              exact="true"
+              activeClassName="active"
+              onClick={() => setShowMenu(false)}
+            >
+              APPOINTMENT
+            </NavLink>
+          </h3>
+          <h3>
+            <NavLink to="contact" onClick={() => setShowMenu(false)}>
+              CONTACT ME
+            </NavLink>
+          </h3>
+        </div>
       </nav>
-      <button className="navContactBtn">
-        <Link to="contact">
-          <img src={contactImg} alt="contact" className="contactBtnImg" />
-          Contact Me
-        </Link>
-      </button>
-    </div>
+    </>
   );
 }
