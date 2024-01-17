028f9f413a207d55b567de0ccff2bba2491931f0
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-01-17 14:36:56 -0500

    update layout imports

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,11 +1,6 @@
 import { Nav } from "./nav/nav.jsx";
 import React from "react";
 import "./layout.css";
-<<<<<<< HEAD
-
-import { Footer } from "../home/footer/footer.jsx";
-=======
->>>>>>> 83a7f5dac64cb8e5627f090a3c634b312c457f33
 import RoutesComponent from "./routes.jsx";
 import { Footer } from "../home/footer/footer.jsx";
 
