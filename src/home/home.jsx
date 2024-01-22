9bedc4e8ac2fec1587dea0c63ad3bf92056ce246
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2024-01-22 10:09:10 -0500

    changes made to home and app

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,13 +1,17 @@
-import Appt from "./appt/appt";
-import Abt from "./abt/abt";
-import Map from "./map/map";
+import  Appt  from './appt/appt';
+import Abt from './abt/abt';
+import  Map  from './map/map';
+import TestimonialSlider from './testimonial/testimonial.jsx';
+import testimonialsData from './testimonial/testimonial.json';
 
-export default function Home() {
-  return (
-    <>
-      <Appt />
-      <Abt />
-      <Map />
-    </>
-  );
+
+export default function Home () {
+    return (
+        <>
+        <Appt />
+        <Abt />
+        <Map />
+        <TestimonialSlider testimonials={testimonialsData} />
+        </>
+    )
 }
